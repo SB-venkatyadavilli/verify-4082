f94b24cc4eb009bf4a2edb510b469c83a91459c7
venkat.yadavilli@smartbear.com <venkat.yadavilli@smartbear.com>
2025-10-08 13:19:28 +0530

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-20523.stoplight-dev.com/api/v1/projects/cHJqOjQyOA/spectral.js?branch_slug=main&token=6a0eeca5-9b24-4271-afd1-61f07925f1fe";
+export default { extends: ruleset };
